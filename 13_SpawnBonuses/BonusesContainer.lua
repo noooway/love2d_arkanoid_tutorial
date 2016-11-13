a86afb2e86ad18643cf84b309ce76f6dc3b12268
noway <noway@debian>
2016-11-13 17:56:06 +0300

    Basic implementation for 13_SpawnBonuses

---
+++ 13_SpawnBonuses/BonusesContainer.lua
@@ -0,0 +1,43 @@
+local Bonus = require "Bonus"
+local vector = require "vector"
+local love = love
+local setmetatable = setmetatable
+local table = table
+local pairs = pairs
+
+local BonusesContainer = {}
+
+if setfenv then
+   setfenv(1, BonusesContainer) -- for 5.1
+else
+   _ENV = BonusesContainer -- for 5.2
+end
+
+function BonusesContainer:new( o )
+   o = o or {}
+   setmetatable(o, self)
+   self.__index = self
+   o.name = o.name or "bonuses_container"   
+   o.bonuses = o.bonuses or {}
+   o.collider = o.collider or {}
+   return o
+end
+
+function BonusesContainer:update( dt )
+   for _, bonus in pairs( self.bonuses ) do
+      bonus:update( dt ) 
+   end   
+end
+
+function BonusesContainer:draw()
+   for _, bonus in pairs( self.bonuses ) do
+      bonus:draw()
+   end   
+end
+
+function BonusesContainer:add_bonus( new_bonus_properties )
+   local new_bonus = Bonus:new( new_bonus_properties )
+   table.insert( self.bonuses, new_bonus )
+end
+
+return BonusesContainer
