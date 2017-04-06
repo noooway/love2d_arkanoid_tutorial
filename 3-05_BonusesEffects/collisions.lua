c4c0e08d78c2aab1bafffe78504a884ef07e8295
ab <noway@ab.home>
2017-04-06 09:15:10 +0300

    Updates in Ch. 3

--- 3-05_BonusesEffects/collisions.lua
+++ 3-05_BonusesEffects/collisions.lua
@@ -12,35 +12,32 @@ function collisions.resolve_collisions( ball, platform,
 end
 
 function collisions.check_rectangles_overlap( a, b )
-   local overlap = false
-   local shift_b = vector( 0, 0 )
-   if not( a.x + a.width < b.x  or b.x + b.width < a.x  or
-	   a.y + a.height < b.y or b.y + b.height < a.y ) then
-      overlap = true
-      if ( a.x + a.width / 2 ) < ( b.x + b.width / 2 ) then
-	 shift_b.x = ( a.x + a.width ) - b.x
-      else 
-	 shift_b.x = a.x - ( b.x + b.width )
-      end
-      if ( a.y + a.height / 2 ) < ( b.y + b.height / 2 ) then
-	 shift_b.y = ( a.y + a.height ) - b.y
-      else
-	 shift_b.y = a.y - ( b.y + b.height )
-      end      
-   end
-   return overlap, shift_b
+   local x_overlap, x_b_shift = collisions.overlap_along_axis(
+      a.center_x, a.halfwidth, b.center_x, b.halfwidth )
+   local y_overlap, y_b_shift = collisions.overlap_along_axis(
+      a.center_y, a.halfheight, b.center_y, b.halfheight )
+   local overlap = ( x_overlap > 0 ) and ( y_overlap > 0 )
+   return overlap, vector( x_b_shift, y_b_shift )
+end
+
+function collisions.overlap_along_axis( a_pos, a_size, b_pos, b_size )
+   local diff = b_pos - a_pos
+   local dist = math.abs( diff )
+   local overlap_value = a_size + b_size - dist
+   local b_shift = diff / dist * overlap_value
+   return overlap_value, b_shift
 end
 
 function collisions.ball_platform_collision( ball, platform )
    local overlap, shift_ball
-   local a = { x = platform.position.x,
-	       y = platform.position.y,
-	       width = platform.width,
-	       height = platform.height }
-   local b = { x = ball.position.x - ball.radius,
-	       y = ball.position.y - ball.radius,
-	       width = 2 * ball.radius,
-	       height = 2 * ball.radius }
+   local a = { center_x = platform.position.x + platform.width / 2,
+	       center_y = platform.position.y + platform.height / 2,
+	       halfwidth = platform.width / 2,
+	       halfheight = platform.height / 2 }
+   local b = { center_x = ball.position.x,
+	       center_y = ball.position.y,
+	       halfwidth = ball.radius,
+	       halfheight = ball.radius }
    overlap, shift_ball =
       collisions.check_rectangles_overlap( a, b )   
    if overlap then
@@ -50,15 +47,15 @@ end
 
 function collisions.ball_walls_collision( ball, walls )
    local overlap, shift_ball
-   local b = { x = ball.position.x - ball.radius,
-	       y = ball.position.y - ball.radius,
-	       width = 2 * ball.radius,
-	       height = 2 * ball.radius }
+   local b = { center_x = ball.position.x,
+	       center_y = ball.position.y,
+	       halfwidth = ball.radius,
+	       halfheight = ball.radius }
    for _, wall in pairs( walls.current_level_walls ) do
-      local a = { x = wall.position.x,
-		  y = wall.position.y,
-		  width = wall.width,
-		  height = wall.height }      
+      local a = { center_x = wall.position.x + wall.width / 2,
+		  center_y = wall.position.y + wall.height / 2,
+		  halfwidth = wall.width / 2,
+		  halfheight = wall.height / 2 }
       overlap, shift_ball =
       	 collisions.check_rectangles_overlap( a, b )
       if overlap then
@@ -69,15 +66,15 @@ end
 
 function collisions.ball_bricks_collision( ball, bricks, bonuses )
    local overlap, shift_ball
-   local b = { x = ball.position.x - ball.radius,
-	       y = ball.position.y - ball.radius,
-	       width = 2 * ball.radius,
-	       height = 2 * ball.radius }
+   local b = { center_x = ball.position.x,
+	       center_y = ball.position.y,
+	       halfwidth = ball.radius,
+	       halfheight = ball.radius }
    for i, brick in pairs( bricks.current_level_bricks ) do   
-      local a = { x = brick.position.x,
-		  y = brick.position.y,
-		  width = brick.width,
-		  height = brick.height }
+      local a = { center_x = brick.position.x + brick.width / 2,
+		  center_y = brick.position.y + brick.height / 2,
+		  halfwidth = brick.width / 2,
+		  halfheight = brick.height / 2 }
       overlap, shift_ball =
       	 collisions.check_rectangles_overlap( a, b )
       if overlap then	 
@@ -89,15 +86,15 @@ end
 
 function collisions.platform_walls_collision( platform, walls )
    local overlap, shift_platform
-   local b = { x = platform.position.x,
-	       y = platform.position.y,
-	       width = platform.width,
-	       height = platform.height }
+   local b = { center_x = platform.position.x + platform.width / 2,
+	       center_y = platform.position.y + platform.height / 2,
+	       halfwidth = platform.width / 2,
+	       halfheight = platform.height / 2 }
    for _, wall in pairs( walls.current_level_walls ) do
-      local a = { x = wall.position.x,
-		  y = wall.position.y,
-		  width = wall.width,
-		  height = wall.height }      
+      local a = { center_x = wall.position.x + wall.width / 2,
+		  center_y = wall.position.y + wall.height / 2,
+		  halfwidth = wall.width / 2,
+		  halfheight = wall.height / 2 }      
       overlap, shift_platform =
       	 collisions.check_rectangles_overlap( a, b )
       if overlap then	 
@@ -108,15 +105,15 @@ end
 
 function collisions.platform_bonuses_collision( platform, bonuses, ball )
    local overlap
-   local b = { x = platform.position.x,
-	       y = platform.position.y,
-	       width = platform.width,
-	       height = platform.height }
+   local b = { center_x = platform.position.x + platform.width / 2,
+	       center_y = platform.position.y + platform.height / 2,
+	       halfwidth = platform.width / 2,
+	       halfheight = platform.height / 2 }
    for i, bonus in pairs( bonuses.current_level_bonuses ) do
-      local a = { x = bonus.position.x - bonuses.radius,
-		  y = bonus.position.y - bonuses.radius,
-		  width = 2 * bonuses.radius,
-		  height = 2 * bonuses.radius }      
+      local a = { center_x = bonus.position.x,
+		  center_y = bonus.position.y,
+		  halfwidth = bonuses.radius,
+		  halfheight = bonuses.radius }      
       overlap = collisions.check_rectangles_overlap( a, b )
       if overlap then
 	 bonuses.bonus_collected( i, bonus, ball, platform )
